58d59a0aba07e0d573209edb0358e9cc9ea203c2
Christine Cua <cloudycua@gmail.com>
2017-10-18 04:17:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def won?(board)
       board[combo[0]] == board[combo[1]] &&
       board[combo[1]] == board[combo[2]] &&
       position_taken?(board, combo[0])
-        return combo
+        true
       else false
     end
   end
