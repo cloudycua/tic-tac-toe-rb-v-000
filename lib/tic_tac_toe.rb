c9a0087c2c7ab8a95364cad575e00daf9dd023f2
Christine Cua <cloudycua@gmail.com>
2017-10-18 04:13:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,9 +57,11 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
+    if
+      board[combo[0]] == board[combo[1]] &&
+      board[combo[1]] == board[combo[2]] &&
+      position_taken?(board, combo[0])
+    return combo  
   end
 end
 
