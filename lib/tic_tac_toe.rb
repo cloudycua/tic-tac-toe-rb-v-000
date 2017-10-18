27f5790ed9be0bbbf3f3bedc9b8d2edd194832af
Christine Cua <cloudycua@gmail.com>
2017-10-18 03:01:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def turn(input)
     index = input_to_index(input)
     if valid_move?(board, index)
       token = current_player(board)
-      move (board, index, token)
+      move(board, index, token)
       display_board(board)
     else
       turn(board)
