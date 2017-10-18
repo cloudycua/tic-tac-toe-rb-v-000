c626feb6a76ba9fb44317cde09ae9aa383bf33cf
Christine Cua <cloudycua@gmail.com>
2017-10-18 01:03:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -24,3 +24,7 @@ end
 def move(board, index, player_token)
   board[index] = player_token
 end
+
+def position_taken(board, index)
+  board[index] != " " && board[index] != "" && board[index] != nil
+end
